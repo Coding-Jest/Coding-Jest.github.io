8b3f04b4d516f02a8166e58bddb5580c9bed94ca
Coding-Jest <pengbinli8663@163.com>
2024-09-06 01:22:56 +0800

    Site updated: 2024-09-06 01:22:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/03/hello-world/","2024/09/03/docker-md/"];function toRandomPost(){
+var posts=["2024/09/03/docker-md/","2024/09/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

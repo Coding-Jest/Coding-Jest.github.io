59f20e121b8efc87304b261772d7c5200f09381e
Coding-Jest <pengbinli8663@163.com>
2024-10-06 02:26:08 +0800

    Site updated: 2024-10-06 02:26:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/03/hello-world/","2024/09/12/maven/"];function toRandomPost(){
+var posts=["2024/09/03/hello-world/","2024/10/06/Failed to connect to github.com port 443 after 21058 ms Couldn't connect to server/","2024/09/12/maven/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
